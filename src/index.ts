9c90d150d18f726c4927a6953b7bc0b7539a7279
Oleksandr Pavlishchev <velogitara@gmail.com>
2025-03-20 01:08:35 +0400

    feat(exercise1): добавляет общий интерфейс TotalPriceParams с опциональными полями isInstallment и months

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
-interface BasicPice {
-  price: number;
-  discount: number;
+interface TotalPriceParams {
+    price: number;
+    discount: number;
+    isInstallment?: boolean;
+    months?: number;
 }
-interface Installment extends BasicPice {
-  isInstallment: true;
-  months: number;
-}
-interface OnePayment extends BasicPice {
-  isInstallment: false;
-  month?: never;
-}
-type TotalPriceParams = Installment | OnePayment;
 
-const totalPrice = (p: TotalPriceParams): number => {
-  const discountPrice = p.price * (1 - p.discount / 100);
-  if (p.isInstallment) {
-    return discountPrice / p.months;
-  }
-  return discountPrice;
+const totalPrice = ({
+    price,
+    discount,
+    isInstallment = false,
+    months,
+}: TotalPriceParams): number => {
+    const discountPrice = price * (1 - discount / 100);
+    if (isInstallment) {
+        const m = months ?? 12;
+        return discountPrice / m;
+    }
+    return discountPrice;
 };
 
 const installmentPrice = totalPrice({
-  price: 100000,
-  discount: 25,
-  isInstallment: true,
-  months: 12,
+    price: 100000,
+    discount: 25,
+    isInstallment: true,
+    months: 12,
 });
 console.log(installmentPrice); // 6250
 
 const oneTimePrice = totalPrice({
-  price: 200000,
-  discount: 60,
-  isInstallment: false,
+    price: 200000,
+    discount: 60,
 });
 console.log(oneTimePrice); // 80000
