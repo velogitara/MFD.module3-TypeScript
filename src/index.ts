49f263df6ac8037ea8999572c219b49b0bc47892
Oleksandr Pavlishchev <velogitara@gmail.com>
2025-03-19 12:50:49 +0400

    Exercise: Total price function

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-import { MY_NUM } from './module.js';
+interface BasicPice {
+  price: number;
+  discount: number;
+}
+interface Installment extends BasicPice {
+  isInstallment: true;
+  months: number;
+}
+interface OnePayment extends BasicPice {
+  isInstallment: false;
+  month?: never;
+}
+type TotalPriceParams = Installment | OnePayment;
 
-console.log(MY_NUM);
+const totalPrice = (p: TotalPriceParams): number => {
+  const discountPrice = p.price * (1 - p.discount / 100);
+  if (p.isInstallment) {
+    return discountPrice / p.months;
+  }
+  return discountPrice;
+};
+
+const installmentPrice = totalPrice({
+  price: 100000,
+  discount: 25,
+  isInstallment: true,
+  months: 12,
+});
+console.log(installmentPrice); // 6250
+
+const oneTimePrice = totalPrice({
+  price: 200000,
+  discount: 60,
+  isInstallment: false,
+});
+console.log(oneTimePrice); // 80000
--- src/module.ts
+++
@@ -1,1 +0,0 @@
-export const MY_NUM = 42;
