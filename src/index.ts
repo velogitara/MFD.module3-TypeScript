9fda4fee7037a0c68ce80b2355752029a60d967e
Oleksandr Pavlishchev <velogitara@gmail.com>
2025-03-20 00:50:45 +0400

    Exercice3: Fetch

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-import { MY_NUM } from './module.js';
+const COMMENTS_URL = 'https://jsonplaceholder.typicode.com/comments?_limit=20';
 
-console.log(MY_NUM);
+interface Comment {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+}
+
+const getData = (url: string): Promise<Comment[]> => {
+    return fetch(url)
+        .then((response: Response): Promise<Comment[]> => {
+            if (!response.ok) {
+                throw new Error(`Ошибка запроса: ${response.status}`);
+            }
+            return response.json() as Promise<Comment[]>;
+        })
+        .catch((error: Error): Comment[] => {
+            console.log(
+                '!!!!!!!!!!Произошла ошибка при выполнении запроса:',
+                error
+            );
+            return [];
+        });
+};
+
+getData(COMMENTS_URL).then((data: Comment[]) => {
+    if (!data || !Array.isArray(data)) {
+        console.log('No data or unexpected format');
+        return;
+    }
+    data.forEach((item: Comment) => {
+        console.log(`ID: ${item.id}, Email: ${item.email}`);
+    });
+});
